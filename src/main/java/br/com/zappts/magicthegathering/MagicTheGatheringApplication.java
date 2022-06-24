8a1987f9bf16d70feb7518f8e0b6cbfbab0a37f6
misael mateus <misaelmateus.java@gmail.com>
2022-06-24 15:57:55 -0300

    Subindo projeto base para o github

---
+++ src/main/java/br/com/zappts/magicthegathering/MagicTheGatheringApplication.java
@@ -0,0 +1,13 @@
+package br.com.zappts.magicthegathering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicTheGatheringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagicTheGatheringApplication.class, args);
+	}
+
+}
